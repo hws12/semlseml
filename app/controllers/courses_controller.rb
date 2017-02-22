d98d0a856b036e8acdb17d395b8440d72abebb77
Nana <braverydream@gmail.com>
2017-02-22 18:32:24 +0800

    init courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,17 @@
+class CoursesController < ApplicationController
+  def seml
+
+  end
+
+  def sm
+
+  end
+
+  def cmmi
+
+  end
+
+  def spm
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # get "students", to: "users#index", as: "students"
   resources :users, :plans
   get "researchscope", to: "plans#researchscope", as: "researchscope"
+  get "courses/seml", to:"courses#seml", as: "seml"
 end
