9a01452534f8a91aaff080dfd96881f9497ba067
Nana <braverydream@gmail.com>
2017-02-23 19:06:05 +0800

    sss

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :stage, :production
-server '', user: 'nana', roles: %w{web app db}
+server '140.118.9.74:2406', user: 'nana', roles: %w{web app db}
 
 
 # role-based syntax
