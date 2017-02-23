088d91bc692593a735e9d7207bd1245584652cc4
Nana <braverydream@gmail.com>
2017-02-23 22:27:35 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, "seml"
-set :repo_url, "git@github.com:hws12/semlseml.git"
+set :repository, "git@github.com:hws12/semlseml.git"
 
 set :deploy_to, '/home/deploy/seml'
 set :user, "nana"
