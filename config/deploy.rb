3ab360b7f950a7bb103ea2611fee93e56be81227
Nana <braverydream@gmail.com>
2017-02-23 22:32:21 +0800

    :)

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url, "git@github.com:hws12/semlseml.git"
 
 set :deploy_to, '/home/deploy/seml'
 set :user, "nana"
+set :use_sudo, true
+
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
